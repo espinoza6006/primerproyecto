cff8c89a1da9ff38c1410a122cca1b248c5b6a9a
Williams <espinoza6006@gmail.com>
2020-12-07 00:53:10 +0100

    Añado todos los archivos que utiliza el paquete de java al repositorio. En esta opción podemos elegiir que archivo subir o no repositorio.

---
+++ PRIMER_PROYECTOGIT/src/primer_proyectogit/PRIMER_PROYECTOGIT.java
@@ -0,0 +1,16 @@
+/*
+PRIMER POYECTO PRUEBA SUBIDO A GIT
+ */
+package primer_proyectogit;
+
+
+public class PRIMER_PROYECTOGIT {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Prueba de subir un proyecto a Github");
+       
+    }
+    
+}
