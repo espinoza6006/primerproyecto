8dd5a63f6d4706590038e8d377a402ddb9e589cf
Williams <espinoza6006@gmail.com>
2020-12-07 18:45:45 +0100

    Añado todos los archivos que utiliza el paquete de java al repositorio. En esta opción podemos elegiir que archivo subir o no repositorio.
    modificación ciclo for

--- PRIMER_PROYECTOGIT/src/primer_proyectogit/PRIMER_PROYECTOGIT.java
+++ PRIMER_PROYECTOGIT/src/primer_proyectogit/PRIMER_PROYECTOGIT.java
@@ -3,14 +3,24 @@ PRIMER POYECTO PRUEBA SUBIDO A GIT
  */
 package primer_proyectogit;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PRIMER_PROYECTOGIT {
 
-    
     public static void main(String[] args) {
-        
+
         System.out.println("Prueba de subir un proyecto a Github");
-       
+
+        for (int i = 5;i<=5 && i>=0; i--){
+            System.out.println(i);
+            try {
+                Thread.sleep(350);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(PRIMER_PROYECTOGIT.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
     }
-    
+
 }
